c6d15084782a21153753287ac40cc57a93264fcd
forrestzach <forrestczach@gmail.com>
2021-07-30 03:00:24 -0400

    Centered personal links and page text, making good progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,10 +14,10 @@ const IndexPage = () => {
 
 
    //I know there has to be a better way to pass this data, don't judge me
-    <Layout pageTitle="Giant Dog" pageText="Man CSS is a bit tough to figure out. Is anything changing still bob???">
-      <body> The wave crashed and hit the sandcastle head-on. The sandcastle began to melt under the waves force and as the wave receded, half the sandcastle was gone. The next wave hit, not quite as strong, but still managed to cover the remains of the sandcastle and take more of it away. The third wave, a big one, crashed over the sandcastle completely covering and engulfing it. When it receded, there was no trace the sandcastle ever existed and hours of hard work disappeared forever</body>
-      <spacer>fack</spacer>
-      <body> honk honk</body>
+    <Layout pageTitle="Giant Dog">
+      <p> The wave crashed and hit the sandcastle head-on. The sandcastle began to melt under the waves force and as the wave receded, half the sandcastle was gone. The next wave hit, not quite as strong, but still managed to cover the remains of the sandcastle and take more of it away. The third wave, a big one, crashed over the sandcastle completely covering and engulfing it. When it receded, there was no trace the sandcastle ever existed and hours of hard work disappeared forever</p>
+      <hr/>
+      <p> honk honk</p>
     </Layout>
   )
 }
