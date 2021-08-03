847612166abfbf0c7a650fd1ccadcdd071ab727c
forrestzach <forrestczach@gmail.com>
2021-08-03 00:07:53 -0400

    Added Personal logo, adding site content

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -3,10 +3,9 @@ import Layout from '../components/layout'
 const ResumePage = () => {
   return (
     <Layout pageTitle="Resume">
-      <h1>Sandcastle Story</h1>
-      <p> The wave crashed and hit the sandcastle head-on. The sandcastle began to melt under the waves force and as the wave receded, half the sandcastle was gone. The next wave hit, not quite as strong, but still managed to cover the remains of the sandcastle and take more of it away. The third wave, a big one, crashed over the sandcastle completely covering and engulfing it. When it receded, there was no trace the sandcastle ever existed and hours of hard work disappeared forever</p>
+      <h1>My Experience</h1>
+      <a href="../files/FZ_Resume_3-29-2021.pdf" download>.PDF of Resume</a>
       <hr/>
-      <p> honk honk</p>
     </Layout>
   )
 }
