aa8244a41a31a09633a61e8431f1507249b3fcd9
forrestzach <forrestczach@gmail.com>
2021-08-18 11:03:53 -0400

    Adding details to Portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,6 +2,7 @@
 import * as React from 'react'
 //import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { Link } from 'gatsby'
 // Step 2: Define your component
 const AboutPage = () => {
   return (
@@ -17,8 +18,12 @@ const AboutPage = () => {
     <h2>Sympto-bot -  DATE HERE</h2>
     <p> sympto-bot content</p>
     <br/>
-    <h2>Puddle - DATE HERE</h2>
-    <p>puddle content</p>
+    <h2>Puddle - January 25th, 2020</h2>
+    <p>iOS app designed alongside a team.  Best described as a Waze for pedestrians <br/>
+      <Link to="https://devpost.com/software/puddle-serfum">
+              DevPost Link
+      </Link>
+    </p>
 
     </Layout>
   )
