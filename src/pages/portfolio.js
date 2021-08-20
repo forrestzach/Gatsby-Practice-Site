960aad8ae2d292b09f64ce2c76a7076d22ba1c4a
forrestzach <forrestczach@gmail.com>
2021-08-20 00:18:37 -0400

    adding more portfolio details

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -15,11 +15,17 @@ const AboutPage = () => {
 
     <hr/>
     <h1>Hackathons</h1>
-    <h2>Sympto-bot -  DATE HERE</h2>
-    <p> sympto-bot content</p>
+    <h2>Sympto-Bot -  January 30th, 2021</h2>
+    <p>React web app which utilized an Apache backend and accessed the Infermedica API.<br/> 
+    <h3>My Role</h3>
+    I was responsible for setting up the Apache web server in Google Cloud, along with setting up the link between our web server and our github repository for easy transfer of new files. I additionally acquired our domain and set up the DNS routing. <br/>WINNER: Best API Use <br/>
+      <Link to="https://devpost.com/software/symptobot">
+              DevPost Link
+      </Link>
+    </p>
     <br/>
     <h2>Puddle - January 25th, 2020</h2>
-    <p>iOS app designed alongside a team.  Best described as a Waze for pedestrians <br/>
+    <p>iOS app designed alongside a team.  Best described as a Waze for pedestrians <br/> WINNER: Most Creative Usage of Radar.io <br/> 
       <Link to="https://devpost.com/software/puddle-serfum">
               DevPost Link
       </Link>
