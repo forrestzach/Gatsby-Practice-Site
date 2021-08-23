16aa09989c8ec8c10c3ace8265ec814360aedad0
forrestzach <forrestczach@gmail.com>
2021-08-23 01:48:13 -0400

    Completed pages and fixed download link

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,24 @@ import Layout from '../components/layout'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Hello, and welcome!">
-      <h1>Sandcastle Story</h1>
-      <p> The wave crashed and hit the sandcastle head-on. The sandcastle began to melt under the waves force and as the wave receded, half the sandcastle was gone. The next wave hit, not quite as strong, but still managed to cover the remains of the sandcastle and take more of it away. The third wave, a big one, crashed over the sandcastle completely covering and engulfing it. When it receded, there was no trace the sandcastle ever existed and hours of hard work disappeared forever</p>
+      <h3>I'm Forrest, and I'm a Software Developer!</h3> <br/>
+      <hr/>
+      <h1>About Me</h1>
+      
+      <p>
+        I like to occupy my time with computers, software, robotics, and logic. <br/>
+        In my career, I am looking into the future to see where software and business intersect.<br/>
+        <h3>Education details:</h3>
+        I'm a Computer Science major at Clemson University, expecting to graduate in 2023. 
+        Additionally, I am also pursuing a Business Administration Minor which has been a lot more interesting than I initially expected.  
+        It has pulled back the curtain on how businesses are run from a variety of perspectives within any given company such as management, financing, and accounting. 
+      </p>
+      <br/>
       <hr/>
       <h1>Experience</h1>
       <h2>Languages/Libraries</h2>
       <h3>Java, C++, C, Python, HTML, CSS, JavaScript, React, GatsbyJS</h3>
+      <br/>
       <h2>Other Technologies</h2>
       <h3>Debian, Apache, NGINX, Flask, Git</h3>
 
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,12 +1,32 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import resumeFile from "../files/FZ_Resume_3-29-2021.pdf"
 const ResumePage = () => {
   return (
     <Layout pageTitle="Resume">
-      <h1>My Experience</h1>
-      <a href={"../files/FZ_Resume_3-29-2021.pdf"} download>.PDF of Resume</a> <br/>
-      <a href={`../files/test.txt`} download>test</a>
+      <h3><a href={resumeFile} download>Download PDF of Resume</a> </h3><br/>    
       <hr/>
+      <h1>Clemson Computing &amp; Information Technology (CCIT) (Current) </h1>
+      <h2>Laptop Support / Service Desk</h2>
+      <p>
+        -Provide in person customer-facing support for software/hardware issues on student’s laptops.
+        <br/>-Worked remotely for 8 months in CCIT’s Tier 1 call center, providing a myriad of support processes for Clemson students and faculty.
+      </p>
+      <br/>
+      <h1>McLeod Information Systems</h1>
+      <h2>Lead Intern 2019</h2>
+      <p>
+        -Led a 6 person team of interns, provided assistance and designated tasks.<br/>
+        -Helped prepare company for future clients.<br/>
+        -Gained extensive knowledge in network architecture and virtual machines. 
+      </p>
+      <br/>
+      <h1>Charleston County School District; Contracts and Procurement Office</h1>
+      <h2>Summer Intern 2018</h2>
+      <p>
+        -1 of 17 selected from Charleston County to participate in a paid work/study program.<br/>
+        -Worked as a data entry clerk, completing multiple major projects necessary for 2018-2019 school year.
+      </p>
     </Layout>
   )
 }
